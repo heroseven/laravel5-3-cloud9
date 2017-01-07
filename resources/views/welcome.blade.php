4d6eb9dc0bd7c1ee23b1481dab082407e0734305
Stephan Vargas Schebesta <nbr920@gmail.com>
2017-01-07 17:37:46 +0000

    success installation of laravel 5.3 and vue.js2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel with Vue.js
                 </div>
 
                 <div class="links">
